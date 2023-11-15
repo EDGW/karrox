d00892996ba144329407d867f652415494b9e827
EDGW <3538722943@qq.com>
2023-11-15 18:48:54 +0800

    Added comments

--- kernel/boot/arch/arch.h
+++ kernel/boot/arch/arch.h
@@ -1,6 +1,7 @@
 #ifndef _ARCH_ARCH_H
 #define _ARCH_ARCH_H
 /*      A public interface of arches        */
+/*      Any boots should implement this interface       */
 namespace arch{
     /*      get a string which describes the boot       */
     const char* getbootinfo();
